df828bbf20bf10c2630d6a2b56d4c1ec4eb38a89
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-07 14:47:03 +0900

    月日の占い結果機能を追加

--- 占いApp/ViewController.swift
+++ 占いApp/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     // ここに関数つくって下で呼び出す
     
     var yearText = ""
+    var monthText = ""
+    var dateText = ""
     
     
     
@@ -57,11 +59,11 @@ class ViewController: UIViewController {
         case "1":
             yearText = "山"
         case "2":
-            yearText = "風"
+            yearText = "湖"
         case "3":
             yearText = "木"
         case "4":
-            yearText = "火"
+            yearText = "森"
         case "5":
             yearText = "太陽"
         case "6":
@@ -69,9 +71,9 @@ class ViewController: UIViewController {
         case "7":
             yearText = "花"
         case "8":
-            yearText = "土"
+            yearText = "草"
         case "9":
-            yearText = "雷"
+            yearText = "雲"
         default:
             print("エラー")
             return
@@ -82,22 +84,76 @@ class ViewController: UIViewController {
         let month =  yyyymmdd[yyyymmdd.index(yyyymmdd.startIndex, offsetBy: 4)..<yyyymmdd.index(yyyymmdd.startIndex, offsetBy: 6)]  // 6,7文字目を取得
         let monthType = month.suffix(1)
         
+        // ケース
+        switch monthType {
+        case "0":
+            monthText = "光"
+        case "1":
+            monthText = "闇"
+        case "2":
+            monthText = "火"
+        case "3":
+            monthText = "水"
+        case "4":
+            monthText = "風"
+        case "5":
+            monthText = "雷"
+        case "6":
+            monthText = "天"
+        case "7":
+            monthText = "氷"
+        case "8":
+            monthText = "土"
+        case "9":
+            monthText = "無"
+        default:
+            print("エラー")
+            return
+        }
+        
+        
         
         
         // 「日」の値を取得
         let date = yyyymmdd.suffix(2)
         let dateType = date.suffix(1)
-        
+        // ケース
+        switch dateType {
+        case "0":
+            dateText = "不思議な雰囲気"
+        case "1":
+            dateText = "粘り強さ"
+        case "2":
+            dateText = "意思の強さ"
+        case "3":
+            dateText = "明るい雰囲気"
+        case "4":
+            dateText = "独特の雰囲気"
+        case "5":
+            dateText = "芯の強さ"
+        case "6":
+            dateText = "神がかっているオーラ"
+        case "7":
+            dateText = "天才のオーラ"
+        case "8":
+            dateText = "経験を糧にする意思"
+        case "9":
+            dateText = "自分を貫き通す意思"
+        default:
+            print("エラー")
+            return
+        }
         
         
         
         // 4. 血液型
         
+        
         // 5. 好きな数
         
         // 6. 呼び出し
         
-        resultTextView.text = "あなたは\(yearText)に"
+        resultTextView.text = "あなたは\(yearText)に愛されています。属性は「\(monthText)」です。あなたからは\(dateText)が感じられます。"
         
         
     }
